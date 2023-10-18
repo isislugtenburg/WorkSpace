5edab71d476af4ab67a39073e3cdf234cb481bec
Guxxtavo2002 <gustavohenriquepj.gh@gmail.com>
2023-10-17 21:53:58 -0300

    up date 17.10

---
+++ JS/Aula00-RaciocinioLogico/Aula02-Variaveis/main.js
@@ -0,0 +1,34 @@
+var nomeDoPrimeiroAluno = "Isis" 
+
+console.log(nomeDoPrimeiroAluno)
+
+let nomeDeAprovados = 10
+console.log(nomeDeAprovados)
+
+console.clear()
+
+const nomeAluno = "Felipe"
+
+console.log(nomeAluno)
+
+// Exibir a soma de dois numeros
+var numero1 = 2;
+
+var numero2 =  4;
+
+var soma = numero1 + numero2;
+
+console.log(soma)
+
+// strings
+var texto1 = "Texto loucao aqui"
+var texto2 = "Continuação do texto loucao aqui"
+var concatenar = texto1 + texto2;
+console.log(concatenar)
+
+// valores booleans
+console.clear()
+
+var osDeVerdadeAGenteSabeQuemE = true;
+
+console.log('5'+5)
