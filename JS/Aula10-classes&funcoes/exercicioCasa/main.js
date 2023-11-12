5b012018c433e4b1951c476f0999818382a7b6eb
Isis <141160845+isislugtenburg@users.noreply.github.com>
2023-11-12 12:39:29 -0300

    Update main.js

--- JS/Aula10-classes&funcoes/exercicioCasa/main.js
+++ JS/Aula10-classes&funcoes/exercicioCasa/main.js
@@ -1,35 +1,27 @@
-// exemplo classe 
-class celular {
-    constructor(marca,memoria,modelo,memoriaRam, cor) {
-        this.marca = marca ; // são madados de atributos
-        this.modelo = modelo ;  // deve ser usado o sinal de igual
-        this.cor = cor ;
-        this.memoria = memoria ; 
-        this.memoriaRam = memoriaRam ;
+class contaBancaria{
+    constructor(nome, idade, salario, sexo, agencia, conta, tipoConta) {
+        this.nome = nome;
+        this.idade = idade;
+        this.salario = salario;
+        this.sexo = sexo;
+        this.agencia = agencia;
+        this.conta = conta;
+        this.tipoConta = tipoConta;
     }
 
-    ligar(){
-        console.log("ligando o celular...")
+    contaPoupança(){
+       return this.salario - 1.5
     }
-    desligar(){
-        if(this.isDesligado === true) {
-            return 'Deligando......'
-    }else{
-        return 'Não vou delisgar seu banana, catapimbas'
+    
+    contaCorrente(){
+        return this.salario - 3.6
     }
+
+    contaEstudante(){
+        return this.salario - 1.2
     }
-    pesquisa(){
-        console.log('Pesquisando........')
-    }    
 }
- //instancial classe
- var motorola = new celular();  
- console.log(motorola.cor = 'amarelo')
- console.log(motorola.marca = 'nokia')
- console.log(motorola.memoria = '256')
- console.log(motorola.memoriaRam = '4')
 
- var motorola = new celular('amarelo', 'nokia 1956', 56, '4', '2600mg')
- console.log(motorola)
-
- var motorola = new celular('positvo', 'jubileu tisti80', 'preto amarelado', 8, '252mb')
+var funcionaria = new contaBancaria('Maria', '18', 3780, 'Feminino', '156', '45689', 'contaCoorente')
+console.log(funcionaria)
+console.log(funcionaria.contaEstudante())
